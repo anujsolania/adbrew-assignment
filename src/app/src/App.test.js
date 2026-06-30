f13769f04628c62c7afd3119028d906a7c9d153f
anuj solania <anujsolania0507@gmail.com>
2026-07-01 02:30:37 +0530

    fix: update default App unit test to match new UI header

--- src/app/src/App.test.js
+++ src/app/src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders list of todos header', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const headerElement = screen.getByText(/List of TODOs/i);
+  expect(headerElement).toBeInTheDocument();
 });
